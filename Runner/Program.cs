b59d2d598c5832b80e22e8152eed132f06c02f22
Swiftly1 <77643169+Swiftly1@users.noreply.github.com>
2021-12-27 22:05:33 +0100

    Runner project

---
+++ Runner/Program.cs
@@ -0,0 +1,9 @@
+﻿using DictionaryList;
+
+var dict = new DictionaryList<int, int>();
+
+var list1 = new List<int> { 1, 2, 3, 4, 5 };
+var list2 = new List<int> { 1, 2, 3, 4, 5 };
+
+dict.Add(list1, 5);
+dict.Add(list2, 5);
