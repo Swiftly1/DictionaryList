de7283148ee288afe3844a1313f71064028d0baf
Swiftly1 <77643169+Swiftly1@users.noreply.github.com>
2021-12-29 12:07:45 +0100

    Setting up Benchmarking env.

---
+++ src/Benchmark/Benchmark1.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using DictionaryList;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class Benchmark1
+    {
+        private DictionaryList<int, int> dlist;
+        private List<(List<int> List, int Value)> Lists = new List<(List<int> List, int Value)>();
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            dlist = new DictionaryList<int, int>();
+            Lists = Benchmark1Data.GetData().ToList();
+        }
+
+        [Benchmark]
+        public int Test()
+        {
+            foreach (var list in Lists)
+                dlist.Add(list.List, list.Value);
+
+            var sum = 0;
+            for (int i = 0; i < Lists.Count; i += 100)
+            {
+                dlist.TryGet(Lists[i].List, out var val);
+                sum += val;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using Benchmark;
+using BenchmarkDotNet.Running;
+
+namespace ListDictionary.Benchmark;
+
+public class Program
+{
+    static void Main()
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll();
+    }
+}
